3ed3bdfb24aebeee2764dd8d2fa470c2e9b42699
Alonso Lagos <a.lagos10@ufromail.cl>
2025-04-25 21:18:19 -0400

    Creacion de archivo login.txt , para simular una database de matriculas y contraseñas. ademas creado los metodos para manejo del archivo, cargarlo y leerlo.
    creado el metodo de login que probablemente se ejecute al inicio del programa, simulando logearse en la app

--- src/main/java/Clases/Main.java
+++ src/main/java/Clases/Main.java
@@ -1,12 +1,64 @@
 package Clases;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+
+
+
     public static void main(String[] args) {
 
+    }
+    public static boolean logicaLogin(String matricula, String contrasenia){
+        try {
+            InputStream is = Main.class.getClassLoader().getResourceAsStream("login.txt"); // input stream representa una corriente de datos de entrada, se usa el class loader para poder luego acceder a sus recursos, en estecaso el login.txt
+            BufferedReader lector = new BufferedReader(new InputStreamReader(is));
+            String linea;
+            while ((linea = lector.readLine()) != null ){
+                String[] partes = linea.split(",");
+                if (partes.length == 2){
+                    if (partes[0].equals(matricula) && partes[1].equals(contrasenia)) {
+                        lector.close();
+                        return true;
+                    }
+                }
+            }
+            lector.close();
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo de usuarios.");
+        }
+        return false;
+    }
 
+    public static void login(){
+        System.out.println("Por favor ingresa el numero de tu matricula");
+        String nombre = scanner.nextLine();
+        System.out.println("Por favor ingresa tu contraseña");
+        String contraseña = scanner.nextLine();
+
+        while(!logicaLogin(nombre,contraseña)){
+            System.out.println("Por favor ingresa datos válidos");
+            System.out.println("Por favor ingresa el numero de tu matricula");
+            nombre = scanner.nextLine();
+            System.out.println("Por favor ingresa tu contraseña");
+            contraseña = scanner.nextLine();
+        }
+        System.out.println("Has iniciado sesión correctamente!");
 
 
 
 
     }
+
+
+
+
+
+
+
+
+
 }
