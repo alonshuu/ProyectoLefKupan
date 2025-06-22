8723c0fcc389a18fe13fa1376ba71f0ed9be5be6
benjamin <benjaminurrea14@gmail.com>
2025-06-22 11:44:57 -0400

    Avance Ayudantía

--- src/main/java/lefkupan/modelo/Ayudantia.java
+++ src/main/java/lefkupan/modelo/Ayudantia.java
@@ -1,4 +1,25 @@
 package lefkupan.modelo;
 
-public class Ayudantia {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ayudantia{
+    private String nombreRamo;
+    private List<RegistroHoras> registrosHoras;
+
+    public Ayudantia(String nombreRamo){
+        this.nombreRamo = nombreRamo;
+        this.registrosHoras = new ArrayList<>();
+    }
+    public String getNombreRamo(){
+        return nombreRamo;
+    }
+    public List<RegistroHoras> getRegistrosHoras(){
+        return registrosHoras;
+    }
+    public void agregarHoras(double cantidad){
+        RegistroHoras registro = new RegistroHoras(LocalDate.now(), cantidad);
+        registrosHoras.add(registro);
+    }
 }
