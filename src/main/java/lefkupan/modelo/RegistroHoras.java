dae4a19fa13e72cf6bc4cffaee534322570721fd
benjamin <benjaminurrea14@gmail.com>
2025-06-22 12:08:28 -0400

    Avance Ayudantía

--- src/main/java/lefkupan/modelo/RegistroHoras.java
+++ src/main/java/lefkupan/modelo/RegistroHoras.java
@@ -1,4 +1,19 @@
 package lefkupan.modelo;
 
+import java.time.LocalDate;
+
 public class RegistroHoras {
+    private LocalDate fecha;
+    private double cantidad;
+
+    public RegistroHoras(LocalDate fecha, double cantidad) {
+        this.fecha = fecha;
+        this.cantidad = cantidad;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public double getCantidad() {
+        return cantidad;
+    }
 }
