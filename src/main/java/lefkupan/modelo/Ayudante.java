a0ea7122b05e8ce49c8c356129d0ba4ec6e0350d
benjamin <benjaminurrea14@gmail.com>
2025-06-22 10:46:55 -0400

    Avance Ayudante

--- src/main/java/lefkupan/modelo/Ayudante.java
+++ src/main/java/lefkupan/modelo/Ayudante.java
@@ -1,4 +1,11 @@
 package lefkupan.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ayudante {
+    private String matricula;
+    private String contrasena;
+    private double horasTrabajadas;
+    private List<Ayudantia> ayudantias;
 }
