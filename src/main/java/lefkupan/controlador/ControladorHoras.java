fbe0b4c1eb50960fb4f6ebf5dff54a5affae33a2
benjamin <benjaminurrea14@gmail.com>
2025-06-22 12:46:39 -0400

    Avance BaseDatos

--- src/main/java/lefkupan/controlador/ControladorHoras.java
+++ src/main/java/lefkupan/controlador/ControladorHoras.java
@@ -1,4 +1,15 @@
 package lefkupan.controlador;
 
-public class ControladorHoras {
+import lefkupan.modelo.Ayudante;
+import lefkupan.modelo.BaseDatos;
+import java.util.List;
+
+public class ControladorLogin {
+    private List<Ayudante> listaAyudantes;
+    public ControladorLogin() {
+        this.listaAyudantes = BaseDatos.cargarAyudantes();
+    }
+    public Ayudante autenticar(String matricula, String contrasena){
+
+    }
 }
