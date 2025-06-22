1d6264311228330209a1ed10ffd853314a803961
benjamin <benjaminurrea14@gmail.com>
2025-06-22 12:13:32 -0400

    Avance BaseDatos

--- src/main/java/lefkupan/modelo/BaseDatos.java
+++ src/main/java/lefkupan/modelo/BaseDatos.java
@@ -1,4 +1,22 @@
 package lefkupan.modelo;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseDatos {
+    private static final String ARCHIVO = "src/main/resources/usuarios.txt";
+
+    public static List<Ayudante> cargarAyudantes() {
+        List<Ayudante> ayudantes = new ArrayList<>();
+
+        try (BufferedReader lector = new BufferedReader(new FileReader(ARCHIVO))) {
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                String matricula = partes[0];
+
+            }
+        }
+    }
 }
