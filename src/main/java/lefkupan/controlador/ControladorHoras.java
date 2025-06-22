5ded5e14c3ab5bf663cd9d153a80de69589867c0
benjamin <benjaminurrea14@gmail.com>
2025-06-22 12:48:28 -0400

    Avance BaseDatos

--- src/main/java/lefkupan/controlador/ControladorHoras.java
+++ src/main/java/lefkupan/controlador/ControladorHoras.java
@@ -10,6 +10,14 @@ public class ControladorLogin {
         this.listaAyudantes = BaseDatos.cargarAyudantes();
     }
     public Ayudante autenticar(String matricula, String contrasena){
-
+        for (Ayudante a : listaAyudantes) {
+            if (a.getMatricula().equals(matricula) && a.getContrasena().equals(contrasena)) {
+                return a;
+            }
+        }
+        return null;
+    }
+    public List<Ayudante> getListaAyudantes() {
+        return listaAyudantes;
     }
 }
