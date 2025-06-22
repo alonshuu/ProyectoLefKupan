e51cd7f55bae1f9a1f527944468295fce18bbf30
benjamin <benjaminurrea14@gmail.com>
2025-06-22 13:56:26 -0400

    Avance menu

--- src/main/java/lefkupan/vista/Menu.java
+++ src/main/java/lefkupan/vista/Menu.java
@@ -3,23 +3,21 @@ package lefkupan.vista;
 import lefkupan.controlador.ControladorHoras;
 import lefkupan.modelo.Ayudante;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Menu {
     private final Scanner scanner = new Scanner(System.in);
     private final Ayudante ayudante;
     private final ControladorHoras controladorHoras;
+    private final Map<Integer, Runnable> acciones = new HashMap<>();
 
-    public Menu(Ayudante ayudante, ControladorHoras controladorHoras){
+    public Menu(Ayudante ayudante, ControladorHoras controladorHoras) {
         this.ayudante = ayudante;
         this.controladorHoras = controladorHoras;
+
+        acciones.put(1, this::registrarHoras);
+        acciones.put(2, this::verPagoEstimado);
+        acciones.put(0, this::salir);
     }
-    public void iniciar(){
-        int opcion = -1;
-        do {
-            System.out.println("--------------------");
-            System.out.println("        MENÚ        ");
-            System.out.println("--------------------");
-        }
-    }
+    public void
 }
