f498d39e094c023377d563c357123d9c95ae8b0b
francisca <f.aguayo03@ufromail.cl>
2025-07-16 01:27:44 -0400

    modificacion de clase ControladorLogin

--- src/main/java/lefkupan/controlador/ControladorLogin.java
+++ src/main/java/lefkupan/controlador/ControladorLogin.java
@@ -1,5 +1,6 @@
 package lefkupan.controlador;
 
+import lefkupan.modelo.usuario.Administrador;
 import lefkupan.modelo.usuario.Ayudante;
 import lefkupan.modelo.HistorialTxt;
 
@@ -11,21 +12,26 @@ import java.util.Scanner;
 public class ControladorLogin { //encargada de autenticar a los usuarios a partir del archivo usuarios.txt
     private static final String ARCHIVO_USUARIOS = "src/main/resources/usuarios.txt";
 
-    public static Ayudante autenticar(String matricula, String contrasena) {
+    public static Object autenticar(String matricula, String contrasena) { //autentica a un usuario y retorna una instacia de ayudante o administrador
         try(Scanner scanner = obtenerLectorUsuarios()) {
             while(scanner.hasNextLine()) {
                 String linea = scanner.nextLine().trim();
                 String[] partes = linea.split(";");
 
-                if(partes.length !=2) continue;
+                if(partes.length !=3) continue;
 
                 String matriculaGuardada = partes[0];
                 String contrasenaGuardada = partes[1];
+                String rolGuardada = partes[2];
 
                 if(matriculaGuardada.equals(matricula) && contrasenaGuardada.equals(contrasena)) {
-                    Ayudante ayudantia = new Ayudante(matricula, contrasena);
-                    HistorialTxt.cargarHistorial(ayudantia);
-                    return ayudantia;
+                    if (rolGuardada.equalsIgnoreCase("ADMINISTRADOR")) {
+                        return new Administrador(matricula);
+                    } else {
+                        Ayudante ayudantia = new Ayudante(matricula, contrasena);
+                        HistorialTxt.cargarHistorial(ayudantia);
+                        return ayudantia;
+                    }
                 }
             }
         } catch (IOException e) {
